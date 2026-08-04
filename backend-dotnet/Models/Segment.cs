d1ac6d1b2a78783087e892b5457eaa1cb3804d95
KingCoder122 <kingcoder122@gmail.com>
2026-08-04 19:33:26 +0530

    Add Entity Framework Core entity models and database schema

---
+++ backend-dotnet/Models/Segment.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend_dotnet.Models
+{
+    [Table("segment_master")]
+    public class Segment
+    {
+        [Key]
+        [Column("segment_id")]
+        public int segmentId { get; set; }
+
+        [Column("segment_name")]
+        public string segmentName { get; set; } = string.Empty;
+    }
+}
